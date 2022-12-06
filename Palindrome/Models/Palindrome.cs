b160e70f27496733420f599ff15a0bb7855f5924
delguanzon <yodelguanzon@gmail.com>
2022-12-06 11:58:20 -0800

    Add Reverse method
    Co-Authored-By: Robert Onstott <roonstott@gmail.com>

--- Palindrome/Models/Palindrome.cs
+++ Palindrome/Models/Palindrome.cs
@@ -3,29 +3,41 @@ using System.Collections.Generic;
 
 namespace Models
 {
-    public class Palindrome
+  public class Palindrome
+  {
+    private string _word;
+    public Palindrome(string word)
     {
-        private string _word;
-        public Palindrome(string word) 
-        {
-            _word = word; 
-        } 
-
-        public bool IsPalindrome() {
-            char[] charArrayReverse = _word.ToCharArray();
-            Array.Reverse(charArrayReverse);
-            string wordReverse = String.Concat(charArrayReverse);
-            Console.WriteLine(wordReverse);
-            if (wordReverse == _word) 
-            {
-                return true;
-            }
-            else 
-            {
-                return false;
-            }
-        }
-
-        
+      _word = word;
     }
+
+    public bool IsPalindrome()
+    {
+    //   char[] charArrayReverse = _word.ToCharArray();
+    //   Array.Reverse(charArrayReverse);
+    //   string wordReverse = String.Concat(charArrayReverse);
+    //   Console.WriteLine(wordReverse);
+      if (Reverse() == _word)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    private string Reverse()
+    {
+      char[] wordArr = _word.ToCharArray();
+      char[] reversed = new char[wordArr.Length];
+      int ctr = 0;
+      for (int i = wordArr.Length - 1; i >= 0; i--)
+      {
+        reversed[ctr] = wordArr[i];
+        ctr++;
+      }
+      return String.Concat(reversed);
+    }
+  }
 }
